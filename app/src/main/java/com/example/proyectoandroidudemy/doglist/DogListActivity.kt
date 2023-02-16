13b1bea49e1b11322e1d339dfbbea95bac09d509
Daniel VG <daniel.valencia@valid.com>
2023-02-15 20:22:04 -0500

    Implementing adapter

--- app/src/main/java/com/example/proyectoandroidudemy/doglist/DogListActivity.kt
+++ app/src/main/java/com/example/proyectoandroidudemy/doglist/DogListActivity.kt
@@ -1,18 +1,27 @@
-package com.example.proyectoandroidudemy
+package com.example.proyectoandroidudemy.doglist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.proyectoandroidudemy.Dog
+import com.example.proyectoandroidudemy.R
 import com.example.proyectoandroidudemy.databinding.ActivityDogListBinding
 
 class DogListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityDogListBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_dog_list)
+        setContentView(binding.root)
 
         val dogList = getFakeDogs()
 
         val recycler = binding.dogRecycler
+        recycler.layoutManager = LinearLayoutManager(this)
+
+        val adapter = DogAdapter()
+        recycler.adapter = adapter
+
+        adapter.submitList(dogList)
     }
 
     private fun getFakeDogs(): MutableList<Dog> {
---
+++ app/src/test/java/com/example/proyectoandroidudemy/doglist/DogAdapterTest.kt
@@ -0,0 +1,5 @@
+package com.example.proyectoandroidudemy.doglist
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class DogAdapterTest
